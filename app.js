d63be148ed65ba102417c33e12f6afdb819220d7
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2026-02-24 17:01:23 -0300

    Implementado botões reativos de curso na pagina inicial e iniciado a criação das funções de busca dos alunos pelo curso

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const cursos = document.getElementById("cursos")
+const container_cursos = document.querySelector('.container-cursos')
+const container_curso = document.querySelector('.container-curso')
+const container_aluno = document.querySelector('.container-aluno')
+
 
 async function lerAlunos() {
     const url = 'https://lion-school-phbo.onrender.com/alunos'
@@ -20,15 +24,52 @@ async function lerCursos() {
     return cursos
 }
 
-async function CriarCurso() {
-    
-    let cursos = lerCursos()
+async function listaAlunosPorCurso(id) {
+    const url = `https://lion-school-phbo.onrender.com/alunos?curso_id=${id}`
 
-    cursos.forEach(curso => {
-        let 
+    const response = await fetch(url)
+    const alunos = await response.json()
+
+    return alunos
+}
+
+async function CriarCursos() {
+
+    let cursos_data = await lerCursos()
+
+    cursos_data.forEach(curso => {
+        let curso_criado = document.createElement("div")
+        let nomeCurso = document.createElement("h1")
+
+        curso_criado.classList.add('curso')
+        curso_criado.id = curso.id
+        nomeCurso.textContent = curso.sigla
+
+        cursos.appendChild(curso_criado)
+        curso_criado.appendChild(nomeCurso)
+
+        curso_criado.addEventListener('click', function () {
+            container_cursos.classList.remove('active')
+            container_curso.classList.add('active')
+        })
+
     });
+}
+
+async function CriarListaAlunos(id) {
+
+    let cursos = await lerCursos()
+
+    if (id = cursos.id) {
+        let nomeCurso = document.createElement('h1')
+        let aluno = await listaAlunosPorCurso(id)
+    }
 
 }
+
+
+
+CriarCursos()
 
 
 
