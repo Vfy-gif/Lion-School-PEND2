9680651579825628947d69877baff419e27463ae
Vfy-gif <vitormiguel.school@gmail.com>
2026-02-27 20:59:10 -0300

    Realizado mais testes com a ferramenta JEST e adicionado uma DevDependency o babel, para corrigir erros nos imports do test

--- test/app.test.js
+++ test/app.test.js
@@ -1,36 +1,42 @@
 /*********************************************************************************************************************
  * Objetivo: Arquivo respnsável pela realização de TESTES UNITÁRIOS (UNIT TEST) do modulo referente
  *      a calculos matemáticos
- * Data: 24/02/2026
+ * Data: 27/02/2026
  * Autor DEV: Vitor Miguel
  * Autor TEST: Vitor Miguel
  * Versão: 1.0
  *********************************************************************************************************************/
 
 //Import do arquivo que será feito o teste unitário
-const requisicoes_front_end = require('../js/reqs.js')
+import { lerCursos, listaAlunosPorCurso, buscarAluno, lerAlunos } from '../js/reqs.js';
 
 test('Validação de requisição na busca dos alunos da API: ', async function() {
-    result = await requisicoes_front_end.lerAlunos()
+    result = await lerAlunos()
 
     expect(Array.isArray(result)).toBe(true)
+    expect(result).not.toBeNull()
+    expect(result).toBeDefined()
 })
 
 test('Validação de requisição na busca dos cursos da API: ', async function() {
-    result = await requisicoes_front_end.lerCursos()
+    result = await lerCursos()
 
     expect(Array.isArray(result)).toBe(true)
+    expect(result).not.toBeNull()
+    expect(result).toBeDefined()
 })
 
 test('Validação de requisição na busca dos alunos por curso da API: ', async function() {
-    result = await requisicoes_front_end.listaAlunosPorCurso(1)
+    result = await listaAlunosPorCurso(1)
 
     expect(Array.isArray(result)).toBe(true)
+    expect(result).not.toBeNull()
+    expect(result).toBeDefined()
 })
 
 test('Validação de requisição na busca dos aluno da API: ', async function() {
 
-    const result = await requisicoes_front_end.buscarAluno(1)
+    const result = await buscarAluno(1)
     expect(result).toBeDefined()
     expect(result).not.toBeNull()
 
